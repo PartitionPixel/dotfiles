3411290fbe9f0eb3f6ef8fd655d6bde1cf728d8a
PartitionPixel <77061434+PartitionPixel@users.noreply.github.com>
2025-11-07 23:24:09 -0500

    Create 01-stow-dotfiles.sh

---
+++ scripts/01-stow-dotfiles.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+DOTFILES_DIR="$HOME/dotfiles"
+REPO_URL="https://github.com/PartitionPixel/ArchLinux.git"
+
+if [ ! -d "$DOTFILES_DIR" ]; then
+  git clone "$REPO_URL" "$DOTFILES_DIR"
+fi
+
+cd "$DOTFILES_DIR"
+sudo pacman -S --noconfirm stow
+stow hypr

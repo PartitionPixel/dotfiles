6a06c88e8c7fae8611fafa5ca37a0ee0f1dee476
PartitionPixel <77061434+PartitionPixel@users.noreply.github.com>
2025-11-08 15:37:49 -0500

    Update 00-system.sh

--- scripts/00-system.sh
+++ scripts/00-system.sh
@@ -7,7 +7,7 @@ mkdir -p ~/Videos/Recording
 sudo pacman -Syu --noconfirm
 sudo pacman -S --noconfirm git base-devel
 sudo pacman -S --noconfirm linux-zen linux-zen-headers
-sudo pacman -S --noconfirm nvidia-open-dkms nvidia-utils lib32-nvidia-open-utils nvidia-settings
+sudo pacman -S --noconfirm nvidia-open-dkms nvidia-utils lib32-nvidia-utils nvidia-settings
 sudo pacman -S --noconfirm swaync
 sudo pacman -S --noconfirm hypridle hyprpaper
 sudo pacman -S --noconfirm polkit

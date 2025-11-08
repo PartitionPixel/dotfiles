784923aec3c2e8c716cd33a06f9634cf9b06a3f3
PartitionPixel <77061434+PartitionPixel@users.noreply.github.com>
2025-11-08 00:32:49 -0500

    Update 00-system.sh

--- scripts/00-system.sh
+++ scripts/00-system.sh
@@ -6,4 +6,5 @@ sudo pacman -S --noconfirm git base-devel
 sudo pacman -S --noconfirm linux-zen linux-zen-headers
 sudo pacman -S --noconfirm nvidia-open-dkms nvidia-open-utils lib32-nvidia-open-utils nvidia-settings
 sudo pacman -S --noconfirm swaync
+sudo pacman -S --noconfirm hypridle hyprpaper
 
